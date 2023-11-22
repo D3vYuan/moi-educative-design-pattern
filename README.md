d47005a29c45d92826b9fe6e4b74f82d7d8db050
TheMaos <yuanjian@hotmail.com>
2023-11-22 14:33:39 +0800

    initial checkin

---
+++ README.md
@@ -0,0 +1,3 @@
+[Educative - Software Design Patterns: Best Practices for Software Developers][educative-url] 
+
+[educative-url]: https://www.educative.io/courses/software-design-patterns-best-practices
---
+++ design/src/main/java/com/design_patterns/creational/builder/Boeing747Builder.java
@@ -0,0 +1,33 @@
+package com.design_patterns.creational.builder;
+
+class Boeing747 implements IAircraft {
+
+}
+
+public class Boeing747Builder extends AircraftBuilder {
+    Boeing747 boeing747;
+
+    @Override
+    public void buildCockpit() {
+
+    }
+
+    @Override
+    public void buildEngine() {
+
+    }
+
+    @Override
+    public void buildBathrooms() {
+
+    }
+
+    @Override
+    public void buildWings() {
+
+    }
+
+    public IAircraft getResult() {
+        return boeing747;
+    }
+}
